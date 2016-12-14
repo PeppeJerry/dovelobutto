cd2a802f6e57d5548c1d5b3201520c9c0311797e
zard99 <keccoz99@gmail.com>
2016-12-14 23:16:18 +0100

    Added button and email classes.

--- result-not-found.php
+++ result-not-found.php
@@ -1,7 +1,11 @@
 <h2>Ci dispiace, non abbiamo trovato l'oggetto <b id="product-searched"></b></h2>
 <h3>Inserisci la tua email e ti contatteremo non appena lo sapremo!</h3>
 <form action="email.php" method="POST" accept-charset="utf-8">
-  <input type="email" name="email">
-  <input type="submit" value="Invia">
+    <span style="display:block;">
+        <input type="email" name="email" id="email-bar">
+    </span>
+    <span style="display:block;margin-top: 15px;">
+        <input type="submit" value="Invia" class="button">
+    </span>
   <input type="hidden" id="product-name" name="product-name" value="">
 </form>
