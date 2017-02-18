60de2449a944cf74ee7965c1295fe33c73948598
Mario Lamacchia <mario.lamacchia@nokia.com>
2017-02-18 13:45:51 +0100

    Move everything to web and remove backend

--- api/result.php
+++
@@ -1,3 +0,0 @@
-<?php
-require_once __DIR__ . '/_result.php';
-echo json_encode($result);
--- partials/header.php
+++
@@ -1,6 +0,0 @@
-<header>
-  <a href="/" class="is-centered">
-    <img src="/assets/images/logo.svg" alt="DoveLoButto">
-    <h1>DoveLoButto</h1>
-  </a>
-</header>
